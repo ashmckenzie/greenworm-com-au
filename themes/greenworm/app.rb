4357aaa99a892be55329053c38d190f71745ddac
Ash McKenzie <ash@greenworm.com.au>
2012-05-15 13:18:14 +1000

    ready to use toadhopper

--- themes/greenworm/app.rb
+++ themes/greenworm/app.rb
@@ -1,7 +1,24 @@
+include Sinatra::Toadhopper::Helpers
+
+CONFIG = YAML.load_file('./config/config.yml')
+
 module Nesta
 
   class App
-    use Rack::Static, :urls => ["/greenworm"], :root => "themes/greenworm/public"
+    use Rack::Static, :urls => [ "/greenworm" ], :root => "themes/greenworm/public"
+
+    set :airbrake, 
+      :api_key => CONFIG['errbit']['api_key'],
+      :notify_host => CONFIG['errbit']['host'],
+      :filters => /password/
+
+    # FIXME: move this out into a plugin
+    #
+    error do
+      set_common_variables
+      post_error_to_airbrake!
+      haml(:error)
+    end unless Nesta::App.development? 
   end  
 
   class Page
